eccbd08c4dbfca232877a1cf8e45de52877ea863
Akasan <daisuke1024akagawa@gmail.com>
2019-09-04 10:18:24 +0900

    chore: example.py

--- example.py
+++ example.py
@@ -3,7 +3,8 @@ from visualizer import Visualizer
 import pickle
 import numpy as np
 import torch
-from imglib.image_io import imshow
+import cv2
+
 
 img_np = np.random.randint(0, 256, (100, 100, 3)) / 255
 img = torch.tensor(img_np)
@@ -14,6 +15,6 @@ visualizer.separate_layers()
 visualizer.describe_layers()
 output = visualizer.check_output(input("Please input layer name >>> "), img)
 output = np.array(output).astype(np.uint8)
-print(img_np.shape, output.shape)
-# imshow(img_np * 255, "hoge")
-# imshow(output * 255, "hoge")
+
+cv2.imshow("original_image", img_np * 255)
+cv2.imshow("output_image", output * 255)
