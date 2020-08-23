fb9c90c24d2bfb042251b1e1c1d3935191e2417f
赤川大空 <daisuke1024akagawa@gmail.com>
2020-08-23 20:44:18 +0900

    add plot function

--- TorchUtils/Trainer/Trainer.py
+++ TorchUtils/Trainer/Trainer.py
@@ -1,13 +1,35 @@
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
+from abc import abstractmedhot, ABCMeta
 
 
-def calculate_accuracy(true_outputs, actual_outputs, batch_size):
-    tmp = true_outputs.max(1)[1] == actual_outputs
-    return float(tmp.sum()) * 100.0 / batch_size
+def calculate_accuracy(outputs, labels):
+    return (outputs.max(1)[1] == labels).sum().item()
 
 
-class MLPTrainer:
+class TrainerBase(metaclass=ABCMeta):
+    @abstractmedhot
+    def fit(self):
+        pass
+
+    @abstractmedhot
+    def predict(self):
+        pass
+
+    @abstractmedhot
+    def save(self):
+        pass
+
+    @abstractmedhot
+    def read(self):
+        pass
+
+    @abstractmedhot
+    def plot_result(self):
+        pass
+
+class MLPTrainer(TrainerBase):
     def __init__(self, model, criterion, optimizer, device=None):
         self.model = model
         self.criterion = criterion
@@ -17,40 +39,60 @@ class MLPTrainer:
         else:
             self.device = "cude" if torch.cuda.is_available() else "cpu"
 
-    def fit(self, train_loader, epochs, reshape_size=None, verbose_rate="epoch", validation_loader=None):
+        self.train_loss_history = []
+        self.train_acc_history = []
+        self.val_loss_history = []
+        self.val_acc_history = []
+
+    def fit(self, train_loader, epochs, reshape_size=None, verbose_rate=1, validation_loader=None):
         for epoch in range(epochs):
             train_loss = 0.0
             train_acc = 0.0
-            val_loss = 0.0
-            val_acc = 0.0
 
-            for i, (images, labels) in enumerate(train_loader):
+            self.model.train()
+
+            for images, labels in train_loader:
                 if type(reshape_size) == tuple:
                     images = images.view(*reshape_size)
 
                 images = images.to(self.device)
                 self.optimizer.zero_grad()
                 outputs = self.model(images)
-                train_acc += calculate_accuracy(outputs, labels, train_loader.batch_size)
                 loss = self.criterion(outputs, labels)
-                train_loss += loss.item() / train_loader.batch_size
                 loss.backward()
                 self.optimizer.step()
 
+                train_acc += calculate_accuracy(outputs, labels)
+                train_loss += loss.item()
+
+            train_loss /= len(train_loader.dataset)
+            train_acc /= len(train_loader.dataset)
+            self.train_loss_history.append(train_loss)
+            self.train_acc_history.append(train_acc)
+
             if not validation_loader is None:
+                val_loss = 0.0
+                val_acc = 0.0
+
+                self.model.eval()
                 with torch.no_grad():
                     for images, labels in validation_loader:
                         if type(reshape_size) == tuple:
                             images = images.view(*reshape_size)
 
                         images = images.to(self.device)
-                        self.optimizer.zero_grad()
                         outputs = self.model(images)
-                        val_acc += calculate_accuracy(outputs, labels, validation_loader.batch_size)
+                        val_acc += calculate_accuracy(outputs, labels)
                         loss = self.criterion(outputs, labels)
-                        val_loss += loss.item() / validation_loader.batch_size
+                        val_loss += loss.item()
 
-            print(f"Epoch [{epoch+1} / {epochs}] accuracy: {train_acc / len(train_loader)} loss: {train_loss / len(train_loader)}")
+                    val_loss /= len(validation_loader.dataset)
+                    val_acc /= len(validation_loader.dataset)
+                    self.val_loss_history.append(val_loss)
+                    self.val_acc_history.append(val_acc)
+
+            if epoch % verbose_rate == 0:
+                print(f"Epoch [{epoch+1} / {epochs}] accuracy: {train_acc} loss: {train_loss}")
 
     def predict(self, test_loader, reshape_size=None):
         for images, labels in test_loader:
@@ -61,9 +103,24 @@ class MLPTrainer:
             outputs = self.model(images)
             loss = self.criterion(outputs, labels)
 
-
     def save(self, model_path="model.pth"):
         torch.save(self.model.state_dict(), model_path)
 
     def read(self, model_path="model.pth"):
         self.model.load_state_dict(torch.load(model_path, map_location=self.device))
+
+    def plot_result(self):
+        # Accuracy
+        plt.subplot(1, 2, 1)
+        plt.plot(self.train_acc_history, label="train", color="r")
+        plt.plot(self.val_acc_history, label="val", color="b")
+        plt.xlabel("epoch")
+        plt.ylabel("Accuracy")
+        plt.legend()
+        plt.subplot(1, 2, 2)
+        plt.plot(self.train_loss_history, label="train", color="r")
+        plt.plot(self.val_loss_history, label="val", color="b")
+        plt.xlabel("epoch")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.show()
--- examples/MLP-MNIST-Test.py
+++ examples/MLP-MNIST-Test.py
@@ -14,13 +14,13 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(28**2, 512),
-            nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
             nn.ReLU(True),
             nn.Linear(512, 256),
-            nn.BatchNorm1d(256),
+            # nn.BatchNorm1d(256),
             nn.ReLU(True),
             nn.Linear(256, 128),
-            nn.BatchNorm1d(128),
+            # nn.BatchNorm1d(128),
             nn.ReLU(True),
             nn.Linear(128, 10),
             nn.Softmax()
@@ -31,14 +31,16 @@ class Model(nn.Module):
 
 if __name__ == "__main__":
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    train_loader, test_loader = load_public_datasets("FashionMNIST", transform=transform)
+    train_loader, test_loader = load_public_datasets("MNIST", transform=transform)
 
     # neurons = [28*28, 256, 128, 10]
     # activations = ["relu", "relu", "softmax"]
     # model = MLP(neurons, activations)
     model = Model()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-5)
+    # optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-5)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
 
     trainer = MLPTrainer(model, criterion, optimizer)
-    trainer.fit(train_loader, 1, reshape_size=(-1, 28**2))
+    trainer.fit(train_loader, epochs=5, reshape_size=(-1, 28**2), validation_loader=test_loader)
+    trainer.plot_result()
