67eb11781b4d611199ae2a14f16da9362ca532c7
Akasan <daisuke1024akagawa@gmail.com>
2020-08-26 18:15:02 +0900

    try torch-lr-finder

--- examples/CNN-MNIST-Test.py
+++ examples/CNN-MNIST-Test.py
@@ -8,6 +8,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
+import torch.nn.functional as F
+from torch_lr_finder import LRFinder
 
 
 class Model(nn.Module):
@@ -17,17 +19,19 @@ class Model(nn.Module):
             nn.Conv2d(1, 16, 3),
             nn.ReLU(True),
             nn.Conv2d(16, 32, 3, padding=1),
-            nn.ReLU(True)
+            nn.ReLU(True),
+            # nn.MaxPool2d(32, 26)
         )
 
         self.classification = nn.Sequential(
-            nn.Linear(21632, 10),
+            nn.Linear(32, 10),
             nn.Softmax()
         )
 
     def forward(self, x):
         x = self.model(x)
-        x = x.view(x.size(0), -1)
+        x = F.max_pool2d(x, kernel_size=26)
+        x = x.view(x.size()[0], 32)
         return self.classification(x)
 
 
@@ -35,9 +39,14 @@ if __name__ == "__main__":
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
     train_loader, test_loader = load_public_dataset("MNIST", transform=transform)
     model = Model()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=1e-7, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
+    lr_finder = LRFinder(model, optimizer, criterion, device="cpu")
+    lr_finder.range_test(train_loader, end_lr=10, num_iter=1000)
+    lr_finder.plot()
+    input()
     trainer = CNNClassificationTrainer(model, criterion, optimizer)
-    check_shape(model, (28, 28, 1))
+    # check_shape(model, (28, 28, 1))
+    # input()
     trainer.fit(train_loader, epochs=5, reshape_size=(-1, 1, 28, 28), validation_loader=test_loader)
     trainer.plot_result()
--- examples/MLP-MNIST-Test.py
+++ examples/MLP-MNIST-Test.py
@@ -8,33 +8,48 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
+from torch_lr_finder import LRFinder
 
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(28**2, 512),
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=3),
+            nn.ReLU(True)
+        )
+        self.classifier = nn.Sequential( 
+            nn.Linear(26*26, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(True),
-            nn.Linear(512, 128),
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(True),
+            nn.Linear(256, 128),
             nn.BatchNorm1d(128),
             nn.ReLU(True),
-            nn.Linear(128, 10),
+            nn.Linear(128, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 10),
             nn.Softmax()
         )
 
     def forward(self, x):
-        return self.model(x)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        return self.classifier(x)
 
 
 if __name__ == "__main__":
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
     train_loader, test_loader = load_public_dataset("MNIST", transform=transform)
     model = Model()
-    check_shape(model, 28**2)
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.Adam(model.parameters(), lr=1e-7, weight_decay=1e-2)
     criterion = nn.CrossEntropyLoss()
-    trainer = MLPClassificationTrainer(model, criterion, optimizer)
-    trainer.fit(train_loader, epochs=10, reshape_size=(-1, 28**2), validation_loader=test_loader)
-    trainer.plot_result()
+    lr_finder = LRFinder(model, optimizer, criterion, device="cpu")
+    lr_finder.range_test(train_loader, end_lr=100, num_iter=100, accumulation_steps=1)
+    lr_finder.plot()
+    lr_finder.reset()
+    # trainer = MLPClassificationTrainer(model, criterion, optimizer)
+    # trainer.fit(train_loader, epochs=10, reshape_size=(-1, 28**2), validation_loader=test_loader)
+    # trainer.plot_result()
