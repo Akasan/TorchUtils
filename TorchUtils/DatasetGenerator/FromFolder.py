49db799549d99d3330a308b02942011a59439c91
Akasan <daisuke1024akagawa@gmail.com>
2020-09-03 14:03:54 +0900

    implement SingleFolderSingleLabelDataset class

--- TorchUtils/DatasetGenerator/FromFolder.py
+++ TorchUtils/DatasetGenerator/FromFolder.py
@@ -1,18 +1,91 @@
+from PIL import Image
+from glob import glob
 import torch
 import numpy as np
 import os
+import random
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
 from ._LoaderGenerator import generate_dataloader as gd
 from ._SplitDataset import split_dataset
 
 
+
+class SingleFolderSingleLabelDataset(torch.utils.data.Dataset):
+    def __init__(self, dir_name, transform=None, label=0, ext="jpg", shuffle=True):
+        """
+        Arguments:
+        ----------
+            dir_name {str} -- root folder of containing images.
+                              Please set this as a absolute path.
+
+        Keyword Arguments:
+        ------------------
+            transform {torchvision.transforms} -- transforms (default: None)
+                                                  If transforms is None, the following transform will be used.
+                                                  transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+            label {int} -- label (default: 0)
+            ext {str} -- image file's extension (default: "jpg")
+            shuffle {bool} -- set True when you want to shuffle the order of images (default: True)
+
+        Examples:
+        ---------
+            >>> custom_dataset = SingleFolderSingleLabelDataset(dir_name="./data",
+        """
+        self.DIR_NAME = dir_name
+        self.LABEL = label
+
+        if transform is None:
+            transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+        self.transform = transform
+        self.files = os.listdir(os.path.abspath(dir_name))
+        self.files = [os.path.join(os.path.abspath(dir_name), f) for f in self.files if f.split(".")[-1] == ext]
+
+        if shuffle:
+            random.shuffle(self.files)
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, i):
+        img = Image.open(self.files[i])
+        img = self.transform(img)
+        return img, self.LABEL
+
+
 def generate_dataset(folder_path, transform):
     return ImageFolder(folder_path, transform)
 
 
-def generate_dataloader(folder_path, transform=None, batch_size=128, shuffle=True, num_workers=2):
-    dataset = generate_dataset(folder_path, transform)
+def generate_dataloader(folder_path, transform=None, batch_size=128, shuffle=True, num_workers=2, is_single_labels=False, ext="jpg", label=0):
+    """ generate_dataloader
+
+    Arguments:
+    ----------
+        folder_path {str} -- folder path
+
+    Keyword Arguments:
+    ------------------
+        transform {torchvision.transforms} -- transform (default: None)
+        batch_size {int} -- batch size (default: 128)
+        shuffle {bool} -- whether data will be shuffled or not (default: True)
+        num_workers {int} -- the number of workers (default: 2)
+        is_single_labels {bool} -- set as True when specified folder only contains data which has the same label (default: False)
+
+    Returns:
+    --------
+        {} -- [description]
+
+    Examples:
+    ---------
+        >>>
+    """
+    if is_single_labels:
+        dataset = SingleFolderSingleLabelDataset(folder_path, transform, label, ext, shuffle)
+    else:
+        dataset = generate_dataset(folder_path, transform)
+
     loader = gd(dataset, batch_size, shuffle, num_workers)
     return loader
 
@@ -33,5 +106,4 @@ def to_cv2_format(img):
 
 
 if __name__ == "__main__":
-    transform = transforms.Compose([transforms.ToTensor()])
-    loader = generate_dataset_loader("C:\\Users\\chino\\Downloads\\31797_40972_bundle_archive\\train", transform, batch_size=1)
+    pass
--- examples/DatasetLoadTest.py
+++ examples/DatasetLoadTest.py
@@ -1,6 +1,20 @@
 import sys
 sys.path.append("../")
 from TorchUtils.DatasetGenerator import FromPublicDatasets
+from TorchUtils.DatasetGenerator import FromFolder
+import cv2
 
-train_loader, val_loader, test_loader = FromPublicDatasets.load_public_dataset_with_val("FashionMNIST")
-print(train_loader, val_loader, test_loader)
+
+loader = FromFolder.generate_dataloader(r"C:\Users\chino\OneDrive\Desktop\ikura-pattern\pattern", is_single_labels=True, ext="jpg", batch_size=1)
+
+
+if __name__ == "__main__":
+    for images, labels in loader:
+        img = images.detach().numpy()[0]
+        img = img.transpose((1, 2, 0))
+        print(img.shape)
+        cv2.imshow("hoge", img)
+        while True:
+            if cv2.waitKey(20) == ord("q"):
+                cv2.destroyAllWindows()
+                break
